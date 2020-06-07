afc8500a23ce8288495e18255f54e63dc8d4d0a5
ArtShp <artemiy.shipovalov2005@gmail.com>
2020-06-07 18:14:41 +0300

    A

--- main.py
+++ main.py
@@ -40,4 +40,5 @@ def run_game():
             gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
             gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
+
 run_game()
